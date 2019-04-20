739ac74407c572761864b8b165d4dad3694e3cec
donald <825870831@qq.com>
2019-04-20 20:28:47 +0800

    feat: 基本可用

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # diet
+
+- [ ] history
--- main.js
+++ main.js
@@ -1,21 +1,59 @@
-function main() {
+const secondsDaily = 24 * 3600
+const data = {
+  weight: +localStorage.getItem('weight'),
+  ratio: +localStorage.getItem('ratio'),
+  calories: +localStorage.getItem('calories'),
+  get caloriesDaily() {
+    const activeIndex = 32
+    return Math.round(this.weight * activeIndex * (1 - (this.ratio / 100)))
+  },
+  get caloriesSecondly() {
+    return Math.round(this.caloriesDaily / secondsDaily)
+  },
+}
+const lastTime = localStorage.getItem('lastTime')
+if (lastTime) data.calories += data.caloriesDaily / secondsDaily * (Date.now() - lastTime) / 1000
+
+function start() {
   const view = {
     weight: document.querySelector('#weight'),
-    height: document.querySelector('#height'),
     ratio: document.querySelector('#ratio'),
     ratioText: document.querySelector('#ratio-text'),
+    caloriesDaily: document.querySelector('#calories-daily'),
     calories: document.querySelector('#calories'),
+    consume: document.querySelector('#consume'),
   }
-  const data = {
-    weight: localStorage.getItem('weight') || 0,
-    height: localStorage.getItem('height') || 0,
-    ratio: localStorage.getItem('ratio') || 0,
-    calories: localStorage.getItem('calories') || 0,
-  }
+
+  view.weight.value = data.weight
+  view.ratio.value = data.ratio
+  view.caloriesDaily.textContent = data.caloriesDaily
+  view.ratioText.textContent = data.ratio + '%'
+
+  document.querySelector('#save').addEventListener('click', () => {
+    data.weight = view.weight.value
+    data.ratio = view.ratio.value
+    view.caloriesDaily.textContent = data.caloriesDaily
+  })
+  document.querySelector('#eat').addEventListener('click', () => {
+    data.calories -= +view.consume.value
+  })
+
   view.ratio.addEventListener('input', e => {
-    console.log(e)
     view.ratioText.textContent = e.target.value + '%'
   })
+
+  setInterval(() => {
+    data.calories += data.caloriesDaily / secondsDaily
+    view.calories.textContent = data.calories.toFixed(2)
+  }, 1000)
 }
 
-document.addEventListener('DOMContentLoaded', main)
+function save() {
+  Object.entries(data).forEach(([k, v]) => {
+    localStorage.setItem(k, v)
+  })
+  localStorage.setItem('lastTime', Date.now())
+}
+
+document.addEventListener('DOMContentLoaded', start)
+window.addEventListener('beforeunload', save)
