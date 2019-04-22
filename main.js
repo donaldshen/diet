37d422600511cb9a847b3952b47c43fd52cc03b1
donald <825870831@qq.com>
2019-04-22 09:58:57 +0800

    feat: 运动系数可调

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const secondsDaily = 24 * 3600
 const data = {
   weight: +localStorage.getItem('weight'),
+  activeIndex: +localStorage.getItem('activeIndex') || 30,
   ratio: +localStorage.getItem('ratio'),
   calories: +localStorage.getItem('calories'),
   get caloriesDaily() {
-    const activeIndex = 32
-    return Math.round(this.weight * activeIndex * (1 - this.ratio / 100))
+    return Math.round(this.weight * this.activeIndex * (1 - this.ratio / 100))
   },
   get caloriesSecondly() {
     return Math.round(this.caloriesDaily / secondsDaily)
@@ -23,6 +23,8 @@ if (typeof data.history === 'string') {
 function start() {
   const view = {
     weight: document.querySelector('#weight'),
+    activeIndex: document.querySelector('#active-index'),
+    activeIndexText: document.querySelector('#active-index-text'),
     ratio: document.querySelector('#ratio'),
     ratioText: document.querySelector('#ratio-text'),
     caloriesDaily: document.querySelector('#calories-daily'),
@@ -30,38 +32,45 @@ function start() {
     consume: document.querySelector('#consume'),
     history: document.querySelector('#history'),
   }
-  const prependHistory = (d, c) => {
+  const insertHistory = (d, c, m = 'append') => {
     const e = document.createElement('div')
     d = new Date(+d).toLocaleString()
     e.innerText = `${d}: ${c}千卡`
-    view.history.prepend(e)
+    view.history[m](e)
   }
 
   view.weight.value = data.weight
+  view.activeIndex.value = data.activeIndex
+  view.activeIndexText.textContent = data.activeIndex
   view.ratio.value = data.ratio
   view.caloriesDaily.textContent = data.caloriesDaily
   view.ratioText.textContent = data.ratio + '%'
   for (let i = 0; i < data.history.length; i += 2) {
-    prependHistory(data.history[i], data.history[i + 1])
+    insertHistory(data.history[i], data.history[i + 1])
   }
+
+  view.activeIndex.addEventListener('input', e => {
+    view.activeIndexText.textContent = e.target.value
+  })
+
+  view.ratio.addEventListener('input', e => {
+    view.ratioText.textContent = e.target.value + '%'
+  })
 
   document.querySelector('#save').addEventListener('click', () => {
     data.weight = view.weight.value
+    data.activeIndex = view.activeIndex.value
     data.ratio = view.ratio.value
     view.caloriesDaily.textContent = data.caloriesDaily
     save()
   })
   document.querySelector('#eat').addEventListener('click', () => {
     const v = +view.consume.value
-    prependHistory(Date.now(), v)
+    insertHistory(Date.now(), v, 'prepend')
     data.history.unshift(Date.now(), v)
     data.calories -= v
     view.consume.value = ''
     save()
-  })
-
-  view.ratio.addEventListener('input', e => {
-    view.ratioText.textContent = e.target.value + '%'
   })
 
   setInterval(() => {
